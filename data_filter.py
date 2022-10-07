6949dd64667f651714e150e8996b5afa4e8ad73c
Luke Ellison <luke.ellison@digicatapult.org.uk>
2022-10-07 18:30:02 +0100

    fixed problem with train test split

--- data_filter.py
+++ data_filter.py
@@ -27,14 +27,23 @@ df = df.loc[df['down_votes'] == 0]
 print(f"rows with no down votes = {len(df)}")
 df = df.loc[df['up_votes'] >= 2]
 print(f"rows with >1 up votes = {len(df)}")
+df['gender'] = df['gender'].str.strip()
 df = df.reset_index()
 print(df.head())
+print(f"male rows = {len(df.loc[df['gender'] == 'male'])}")
+print(f"female rows = {len(df.loc[df['gender'] == 'female'])}")
+test_df = df.loc[df['gender'] != 'male']
+test_df = test_df.loc[test_df['gender'] != 'female']
+print(f'non-male, non-female rows = {len(test_df)}')
+print(df.head())
 
-def write_output(df, name):
+
+def write_output(temp_df, name):
     print(f'Writing "{name}" data...')
     file = open(f'data/{name}/train.tsv', 'w', newline='')
     writer = csv.writer(file, delimiter='\t', lineterminator='\n')
-    train_thresh = len(df)*8.0/10
+    train_thresh = len(temp_df)*8.0/10
+    print(f"train thresh = {train_thresh}")
     file_changed = False
     counts = {
         'train': {
@@ -46,7 +55,7 @@ def write_output(df, name):
             'female': 0,
         }
     }
-    for i,row in tqdm(df.iterrows(), total=len(df)):
+    for i,row in tqdm(temp_df.iterrows(), total=len(temp_df)):
         if not file_changed and i > train_thresh:
             file.close()
             file = open(f'data/{name}/test.tsv', 'w', newline='')
@@ -58,7 +67,11 @@ def write_output(df, name):
     print(counts)
 
 write_output(df, "mixed")
-write_output(df.loc[df['gender'] == 'female'], 'female')
-write_output(df.loc[df['gender'] == 'male'], 'male')
+female_df = df.loc[df['gender'] == 'female']
+female_df = female_df.reset_index()
+write_output(female_df, 'female')
+male_df = df.loc[df['gender'] == 'male']
+male_df = male_df.reset_index()
+write_output(male_df, 'male')
 # train = 2.864, test = 3.431
 # {'train': {'male': 503781, 'female': 175893}, 'test': {'male': 129688, 'female': 37803}}
