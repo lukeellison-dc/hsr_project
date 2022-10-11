7ced71157d18c7081e0aae520a33fe8810eb3bb8
Luke Ellison <luke.ellison@digicatapult.org.uk>
2022-10-11 17:50:57 +0100

    try again

--- asr_trainer.py
+++ asr_trainer.py
@@ -201,18 +201,19 @@ class DataLoader():
         self.length = {}
 
     def load(self, root="/raid/lellison_data/hsr_project/processed/"):
-        for phase in ['train']:
+        for phase in ['train', 'test']:
             print(f'Loading dataset for {self.gender},{phase}...')
             with lzma.open(f'{root}{self.gender}/{phase}.xz','rb') as infile:
                 self.data[phase] = pickle.load(infile)
 
-        self.length[phase] = len(self.data)
+        self.length[phase] = len(self.data[phase])
         print(self.length)
         print(f'Loading datasets.')
     
     def batch_generator(self, phase, batch_size=64):
         keys = ['input_values', 'sentences', 'targets']
-        for i in range(0, len(self.data[phase]), batch_size):
+        for i in range(0, self.length[phase], batch_size):
+            print(f"batch i={i}")
             batch = {
                 x: self.data[phase][x][i:i+batch_size] for x in keys
             }
